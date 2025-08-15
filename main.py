30b594ea830b87e0c5a964c72a16350296bf2142
junturIK <iskandernasyrov55@gmail.com>
2025-08-15 23:18:30 +0500

    download files

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def menu():
+ context = {
+  "title": "меню",
+
+
+  "link vk": "https://vk.com/id612168895",
+  "put": "../static/css/style.css",
+  "fon": "../static/image/фон сталкера.jpg",
+  "audio": "../static/image/Studiya_stalker_-_Stalker_zov_pripyati_koncovka_62816187.mp3",
+  "loin": "https://vk.com/id612168895",
+
+ }
+ return render_template("stalker_menu.html", **context)
+
+
+@app.route('/yupiter/')
+def yupiter():
+ return render_template('stalker_yupiter.html')
+
+
+@app.route('/zaton/')
+def zaton():
+ return render_template('stalker_zaton.html')
+
+@app.route('/pripat/')
+def pripat():
+ return render_template('prypat.html')
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+ app.run()
